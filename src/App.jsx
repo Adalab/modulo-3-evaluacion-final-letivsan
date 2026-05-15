9f4906704a215704b8006560579997b44ea64425
Leti <letivsan@gmail.com>
2026-05-15 12:23:47 +0200

    added structure html without logo

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,36 @@ import "./App.scss";
 
 function App() {
   return (
-    <div>
-      <header>
-        <h1>Título</h1>
+    <>
+      <header className="header">
+        <h1 className="header__title">Personajes de Harry Potter(usar FOTO)</h1>
       </header>
-      <main>
-        <section>
-          <h2>HTML</h2>
-          <p>Aquí va mi HTML</p>
+      <main className="main">
+        <section className="characters">
+          <h2 className="characters__title">Listado de personajes</h2>
+
+          <ul className="characters__list">
+            <li className="card">
+              <img 
+              className="card__img"
+              src="https://placehold.co/210x295/ffffff/666666/?format=svg&text=Harry+Potter"
+              alt="Personaje de Harry Potter"
+              />
+
+              <div className="card__info">
+                <h3 className="card__name">
+                  Harry Potter
+                </h3>
+                <p className="card__species">Humano</p>
+              </div>
+            </li>
+          </ul>
         </section>
       </main>
-    </div>
+    </>
   );
 }
 
 export default App;
+
+
