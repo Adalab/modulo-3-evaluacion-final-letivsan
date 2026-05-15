55ff2d9e847cec5702a6b73f7e64dd0be3fa936f
Leti <letivsan@gmail.com>
2026-05-15 20:04:49 +0200

    Add name filter and adjust character cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,107 @@
-import { useEffect, useState } from "react";
-import Logo from "./images/harry-potter-logo.png";
-import "./App.scss";
+import { useEffect, useState } from 'react';
+import Logo from './images/harry-potter-logo.png';
+import './App.scss';
 
 function App() {
+  /*Sección de Variables de estado*/
+  const [characters, setCharacters] = useState([]);
+  const [searchName, setSearchName] = useState('');
 
-/*Sección de Variables de estado*/
-const [characters, setCharacters] = useState([]);
+  /*Sección de useEffect*/
+  useEffect(() => {
+    fetch('https://hp-api.onrender.com/api/characters/house/gryffindor')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log('Datos originales de la API:', data);
 
+        const cleanData = data.map((character) => {
+          return {
+            id: character.id,
+            name: character.name,
+            species: character.species,
+            image:
+              character.image ||
+              'https://placehold.co/210x295/ffffff/666666/?format=svg&text=Harry+Potter',
+          };
+        });
 
-/*Sección de useEffect*/
-useEffect(() => {
-  fetch('https://hp-api.onrender.com/api/characters/house/gryffindor')
-  .then((res) => res.json())
-  .then((data) => {
-    console.log('Datos originales de la API:', data);
+        console.log('Datos limpios:', cleanData);
 
-    const cleanData = data.map((character) => {
-      return {
-        id: character.id,
-        name: character.name,
-        species: character.species,
-        image: character.image || 'https://placehold.co/210x295/ffffff/666666/?format=svg&text=Harry+Potter',
-      };
-    });
+        setCharacters(cleanData);
+      });
+  }, []);
 
-    console.log('Datos limpios:', cleanData);
+  useEffect(() => {
+    console.log('Estado de characters actualizado:', characters);
+  }, [characters]);
 
-    setCharacters(cleanData);
-  });
-}, []);
+  /*Sección de funciones de eventos*/
+  const handleSearchName = (ev) => {
+    setSearchName(ev.target.value);
+  };
 
-useEffect(() => {
-  console.log('Estado de characters actualizado:', characters);
-}, [characters]);
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+  };
 
-/*Sección de funciones de eventos*/
+  /*Sección de funciones helper para pintar el HTML*/
 
-/*Sección de funciones helper para pintar el HTML*/
-  const renderCharacters = characters.map((character) => {
-    return (
-      <li className="card" key={character.id}>
-        <img
-          className="card__img"
-          src={character.image}
-          alt={`Foto de ${character.name}`}
-          title={`Foto de ${character.name}`}
-        />
-
-        <div className="card__info">
-          <h3 className="card__name">{character.name}</h3>
-          <p className="card__species">{character.species}</p>
-        </div>
-      </li>
-    );
-  });
+  const filteredCharacters = characters.filter((character) =>
+    character.name.toLowerCase().includes(searchName.toLowerCase()),
+  );
 
   return (
-    <>
+    <div className="page">
       <header className="header">
-        <h1 className="header__title">Personajes de Harry Potter</h1>
-        <img 
-        className="header__logo"
-        src={Logo}
-        alt="Harry Potter Logo"
-        />
+        <img className="header__logo" src={Logo} alt="Harry Potter Logo" />
       </header>
 
       <main className="main">
-        <section className="characters">
-          <h2 className="characters__title">Listado de personajes</h2>
+        <form className="filters" onSubmit={handleSubmit}>
+          <label className="filters__label" htmlFor="searchName">
+            Buscar por personaje:
+          </label>
 
-          <ul className="characters__list">{renderCharacters}</ul>
+          <input
+            className="filters__input"
+            type="text"
+            name="searchName"
+            id="searchName"
+            value={searchName}
+            onInput={handleSearchName}
+            placeholder="Ej: Hagrid"
+          />
+        </form>
+
+        <section className="characters">
+          <h2 className="characters__title">Personajes de Gryffindor</h2>
+
+          {filteredCharacters.length === 0 ? (
+            <p className="characters__message">
+              No hay ningún personaje que coincida con la palabra {searchName}.
+            </p>
+          ) : (
+            <ul className="characters__list">
+              {filteredCharacters.map((character) => (
+                <li className="card" key={character.id}>
+                  <img
+                    className="card__img"
+                    src={character.image}
+                    alt={`Foto de ${character.name}`}
+                  />
+
+                  <div className="card__info">
+                    <h2 className="card__name">{character.name}</h2>
+                    <p className="card__species">{character.species}</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
         </section>
       </main>
-    </>
+    </div>
   );
 }
 
 export default App;
-
-
